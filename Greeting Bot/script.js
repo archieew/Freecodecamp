3f1c857ae1a25612a3ac8e03b8832fd941793d8b
Archie Custodio <archie06custodio@gmail.com>
2025-11-17 01:35:19 +0800

    Update script.js

--- Greeting Bot/script.js
+++ Greeting Bot/script.js
@@ -1,1 +1,15 @@
-script.js
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
