c71b09cf347579686de1b73ef836559dfaa0e3b1
Archie Custodio <archie06custodio@gmail.com>
2025-11-20 04:15:48 +0800

    Update script.js

--- Sentence Maker/script.js
+++ Sentence Maker/script.js
@@ -1,1 +1,29 @@
-script.js
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "tiny";
+noun = "dragon";
+verb = "angry";
+place = "mystic cave";
+adjective2 = "sparkly";
+noun2 = "marshmallows";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+
+console.log("First story: " + firstStory);
+
+adjective = "gigantic";
+noun = "unicorn";
+verb = "laughing";
+place = "enchanted forest";
+adjective2 = "glowing";
+noun2 = "cupcakes";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
