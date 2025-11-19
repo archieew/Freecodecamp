210485d5557e4548598ca8d27f7c4e6b9f61625a
Archie Custodio <archie06custodio@gmail.com>
2025-11-20 04:14:03 +0800

    Update script.js

--- Trivia Bot/script.js
+++ Trivia Bot/script.js
@@ -1,1 +1,21 @@
-script.js
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "CodeBot";
+let botLocation = "the JavaScript universe";
+let favoriteLanguage = "JavaScript";
+
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+
+let codingFact = favoriteLanguage + " was created in just 10 days!";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is used to make websites interactive.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " can run both in the browser and on servers using Node.js!";
+console.log(codingFact);
+
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
