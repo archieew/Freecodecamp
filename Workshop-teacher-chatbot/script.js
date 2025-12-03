b822530a77ced7708fc89a4b20199ebe6899c7f1
Archie Custodio <archie06custodio@gmail.com>
2025-12-04 06:33:42 +0800

    keep learning

--- Workshop-teacher-chatbot/script.js
+++ Workshop-teacher-chatbot/script.js
@@ -5,5 +5,8 @@ const botName = "teacherBot";
 const greeting = `My name is ${botName}.`;
 console.log(greeting);
 
-const subject ="JavaScript"
-const topic ="strings"
+const subject = "JavaScript";
+const topic = "strings";
+
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence)
