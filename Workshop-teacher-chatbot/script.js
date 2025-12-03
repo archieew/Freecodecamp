9c334b0914f91920d3177b2b3ee6043cd5f33382
Archie Custodio <archie06custodio@gmail.com>
2025-12-04 06:30:50 +0800

    Update script.js

--- Workshop-teacher-chatbot/script.js
+++ Workshop-teacher-chatbot/script.js
@@ -3,4 +3,7 @@ console.log("Hi there!");
 const botName = "teacherBot";
 
 const greeting = `My name is ${botName}.`;
-console.log(greeting)
+console.log(greeting);
+
+const subject ="JavaScript"
+const topic ="strings"
