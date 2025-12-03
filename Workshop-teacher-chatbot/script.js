552b304b532d5ca1cf8411faca978362d94fe390
Archie Custodio <archie06custodio@gmail.com>
2025-12-04 06:36:53 +0800

    next challenge

--- Workshop-teacher-chatbot/script.js
+++ Workshop-teacher-chatbot/script.js
@@ -9,4 +9,7 @@ const subject = "JavaScript";
 const topic = "strings";
 
 const sentence = `Today, you will learn about ${topic} in ${subject}.`;
-console.log(sentence)
+console.log(sentence);
+
+const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`
+console.log(strLengthIntro);
